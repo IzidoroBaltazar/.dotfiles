2f52605da00051bc2b1663b94ad74329c44d0b34
Pijak, Martin (mp5516) <mp5516@intl.att.com>
2017-02-03 15:53:42 +0100

    New go* aliases, new tmux style

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -13,3 +13,16 @@ alias ll='ls -la'
 alias grep="grep $GREP_OPTIONS"
 unset GREP_OPTIONS
 
+alias vpn='sudo openvpn /etc/openvpn/frinx.ovpn'
+alias stopVpn='sudo pkill openvpn'
+
+alias goUk='ssh -X mp5516@135.76.180.170'
+alias goDe='ssh -X mp5516@135.76.168.170'
+
+alias dockerbuild='docker build --build-arg http_proxy="http://135.28.13.11:8888" --build-arg https_proxy="http://135.28.13.11:8888"'
+alias cpwd='openssl rand -base64'
+
+alias ctrlc='xclip -selection c'
+alias ctrlv='xclip -selection c -o'
+
+alias venv3='mkvirtualenv -p python3'
