f60724031ef87d6762904cf062483f25ef5d0aa8
Pijak, Martin (mp5516) <mp5516@intl.att.com>
2017-05-11 09:59:15 +0200

    [FEATURE] ctrl-r search

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -1,6 +1,10 @@
 STSIZE=5000000
 HISTFILE=~/.zsh_history
 SAVEHIST=5000000
+
+bindkey -v
+bindkey '^R' history-incremental-search-backward
+
 setopt appendhistory
 setopt sharehistory
 setopt incappendhistory
