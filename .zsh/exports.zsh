f93c49ac8124d4b45f9d00982814be194cbc5e14
Martin Pijak (mp5516) <mp5516@intl.att.com>
2019-09-08 16:04:58 +0200

    [FEATURE] use miniconda by default

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -8,10 +8,21 @@
 export GREP_OPTIONS='--color=auto'
 
 # Redefining PATH
-#PATH=$GOPATH/bin:$PATH
-
-export WORKON_HOME=~/virtualenvs
-source ~/.local/bin/virtualenvwrapper.sh
-export PIP_VIRTUALENV_BASE=~/virtualenvs
+# PATH=$PATH:/home/${USER}/miniconda/bin
 
 export EDITOR=nvim
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="${HOME}/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
