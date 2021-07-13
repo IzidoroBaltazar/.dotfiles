ff649ff90b24bf68e132b97a825fcdd5200db920
Martin Pijak <mpijak@outlook.com>
2021-07-13 18:50:23 +0200

    Disable certificate

--- .zsh_conf/exports.zsh
+++ .zsh_conf/exports.zsh
@@ -2,7 +2,7 @@
 #export GOPATH=$HOME/Documents/workspace/gocode
 
 # Environment variables for Java
-#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+# export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 export PATH=$PATH:${HOME}/anaconda3/bin:/snap/bin/:${HOME}/balena-cli
 
@@ -12,4 +12,4 @@ export LC_LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8  
 export LANG=en_US.UTF-8
-export NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt
+# export NODE_EXTRA_CA_CERTS=/usr/local/share/ca-certificates/ca.crt
